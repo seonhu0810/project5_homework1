e18bdaa83ecfa19966af8a5f24a7a7e65d9916a8
seonhu <seonhu0810@gmail.com>
2024-09-04 23:41:42 +0900

    fetchall.py 파일 수정

--- fetchall.py
+++ fetchall.py
@@ -1,7 +1,7 @@
 from mysql.connector import MySQLConnection, Error
 from config import read_config
 
-def query_with_fetchall(config):
+def query_with_fetchall():
     try:
         # 데이터베이스 설정을 읽어옴
         config = read_config()
@@ -9,7 +9,7 @@ def query_with_fetchall(config):
         # 제공된 설정을 사용하여 MySQL 데이터베이스에 연결
         conn = MySQLConnection(**config)
         
-        # 데이터베이스와 상호작용할 커서를 생성
+        # 데이터베이스와 상호작용할 커서를 생성합니다
         cursor = conn.cursor()
         
         # 'books' 테이블에서 모든 행을 조회하는 SELECT 쿼리를 실행
@@ -36,7 +36,5 @@ def query_with_fetchall(config):
         conn.close()
 
 if __name__ == '__main__':
-    # 설정을 읽어옴
-    config = read_config()
-    # 조회 쿼리를 실행할 때 읽어온 설정을 사용
-    query_with_fetchall(config)
+    # 조회 쿼리를 실행할 때 설정을 직접 읽어옴
+    query_with_fetchall()
